ee34e7bf6f39db56b4f779e001b9bec747cf6b71
Michael George <georgem@wlwv.k12.or.us>
2020-02-28 09:25:12 -0800

    commit

--- Card.java
+++ Card.java
@@ -1,7 +1,7 @@
 public class Card{
-    public static final String[] RANK = ("A", "K", "Q", "J", "10", "9", "8", "7","6", "5", "4", "3", "2");
+    public static final String[] RANK = {"A", "K", "Q", "J", "10", "9", "8", "7","6", "5", "4", "3", "2"};
 
-    public static final String[] SUIT = ("♧", "♡", "♢", "♤");
+    public static final String[] SUIT = {"♧", "♡", "♢", "♤"};
 
     public Card(String rank, String suit){
         this.rank = rank;
@@ -17,9 +17,29 @@ public class Card{
     }
 
     public int compareTo(Object other){
-
+        Card otherCard = (Card) other;
+        return this.getRankValue() - otherCard.getRankValue();
     }
+
+    public int getRankValue(){
+        for(int i = 0; i < RANK.length; i++){
+            if(rank.equals(RANK[i])){
+                return 14 - i;
+                }
+            }
+            return -i;
+    }
+
     public String toString(){
-
-    }
+        return rank + suit;
 }
+
+    public static void main(String[] args){
+        Card c1 = new Card(Rank[0], SUIT[0]);
+        System.out.println(c1);
+        Card c2 = new Card(Rank[RANK.length-1], SUIT[SUIT-1]);
+        System.out.println(c2);
+        System.out.println(c1.getRankValue());
+        System.out.println(c2.getRankValue());
+        System.out.println(c1.compareTo());
+    }
---
+++ Deck.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Deck{
+    private ArrayList<Card> deck;
+
+    public Deck(){
+        deck = new ArrayList<>();
+        for(String rank; Card.RANK){
+
+        }
+    }
+}
