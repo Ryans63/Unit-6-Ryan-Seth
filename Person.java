2f349f86cf800d176c045c5475765d67d2cec397
Ryans63 <54951373+Ryans63@users.noreply.github.com>
2020-03-05 19:03:00 -0800

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,15 @@
 public class Player{
-
+private int age;
+private int name;
+  
+  public player(int age, int name){
+    int age = myAge;
+    int name = myName;
+  }
+  public getAge(){
+    this.age = Age;
+  }   
+  public getName(){
+    this.name = Name;
+   }
 }
